b005691a333c5d83488a9ca09577c5e7f2e583d3
Paul Burkinshaw <paul.burkinshaw@gmail.com>
2017-01-08 13:23:42 +0000

    Adding first test

--- PriceCalculation.Tests/BasketTests.cs
+++ PriceCalculation.Tests/BasketTests.cs
@@ -1,14 +1,31 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using NUnit.Framework;
+using System;
 
 namespace PriceCalculation.Tests
 {
-    [TestClass]
+    [TestFixture]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        [Test]
+        public void AddBasketItem_SingleItem_ItemIsAdded()
         {
+            // Arrange
+            var item = new BasketItem { Name = "Butter", Cost = 0.80M, Quantity = 1 };
+            var basket = new Basket();
+
+            var expected = new List<BasketItem>
+            {
+                item
+            };
+
+            // Act
+            basket.AddBasketItem(item);
+            var result = basket.BasketItems;
+
+            // Assert
+            Assert.AreEqual(expected.Count, result.Count);
+            
+
         }
     }
 }
