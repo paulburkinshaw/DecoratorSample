f11611e01188a11c03d10396e2de67c51ac6d82f
Paul Burkinshaw <paul.burkinshaw@gmail.com>
2017-01-08 22:06:37 +0000

    BuyTwoItemsGetDiscountDecorator now applies multiple discounts depending on the number of source items

--- PriceCalculation/BuyTwoItemsGetDiscountDecorator.cs
+++ PriceCalculation/BuyTwoItemsGetDiscountDecorator.cs
@@ -21,12 +21,16 @@ namespace PriceCalculation
         public override decimal CalculateBasketTotal()
 	    {
             decimal total = base.CalculateBasketTotal();
-           
+
             decimal discountPrice = 0.00M;
-           
-            if (base.BasketItems.Exists(x => x.Name == _sourceItem && x.Quantity >= 2))              
-                discountPrice = base.BasketItems.Exists(x => x.Name == _targetItem) ? (BasketItems.Find(x => x.Name == _targetItem).Cost * 50 / 100) : 0.00M;
-            
+            int discounts = 0;
+
+            if (base.BasketItems.Exists(x => x.Name == _sourceItem && x.Quantity >= 2))
+            {
+                int sourceCount = base.BasketItems.Find(x => x.Name == _sourceItem).Quantity;
+                for (int i = 1; i <= sourceCount; i++) { if (i % 2 == 0) discounts++; }
+                discountPrice = base.BasketItems.Exists(x => x.Name == _targetItem) ? (BasketItems.Find(x => x.Name == _targetItem).Cost * 50 / 100) * base.BasketItems.Find(x => x.Name == _targetItem).Quantity : 0.00M;
+            }
 
             return total - discountPrice;
 	    }
