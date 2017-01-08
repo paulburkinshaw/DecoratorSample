602054e710a63a7313caf7d7e2cc8c0edd991282
Paul Burkinshaw <paul.burkinshaw@gmail.com>
2017-01-08 22:00:07 +0000

    Adding BuyTwoItemsGetDiscountDecorator

--- PriceCalculation.Tests/BasketTests.cs
+++ PriceCalculation.Tests/BasketTests.cs
@@ -128,7 +128,7 @@ namespace PriceCalculation.Tests
         }
 
 
-        
+        //
 
         #endregion
     }
---
+++ PriceCalculation/BuyTwoItemsGetDiscountDecorator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceCalculation
+{
+    public class BuyTwoItemsGetDiscountDecorator : BasketTotalDecorator
+    {
+        string _sourceItem;
+        string _targetItem;
+
+        public BuyTwoItemsGetDiscountDecorator(string sourceItem, string targetItem, IBasketTotalCalculator basketTotalCalculator)
+            : base(basketTotalCalculator)
+	    {
+            this._sourceItem = sourceItem;
+            this._targetItem = targetItem;
+	    }
+
+        public override decimal CalculateBasketTotal()
+	    {
+            decimal total = base.CalculateBasketTotal();
+           
+            decimal discountPrice = 0.00M;
+           
+            if (base.BasketItems.Exists(x => x.Name == _sourceItem && x.Quantity >= 2))              
+                discountPrice = base.BasketItems.Exists(x => x.Name == _targetItem) ? (BasketItems.Find(x => x.Name == _targetItem).Cost * 50 / 100) : 0.00M;
+            
+
+            return total - discountPrice;
+	    }
+    }
+}
