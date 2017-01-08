d93a8e1776e26dab90824b1d38b90dc563ec707d
Paul Burkinshaw <paul.burkinshaw@gmail.com>
2017-01-08 19:58:57 +0000

    CalculateBasketTotal now incorporates milk discount

--- PriceCalculation/Basket.cs
+++ PriceCalculation/Basket.cs
@@ -28,12 +28,20 @@ namespace PriceCalculation
 
         public decimal CalculateBasketTotal()
         {
-           
+
+            decimal totalDiscount = 0.00M;
+
             decimal breadDiscount = 0.00M;
-            if(BasketItems.Exists(x => x.Name == "Butter" && x.Quantity ==2))
+            if (BasketItems.Exists(x => x.Name == "Butter" && x.Quantity == 2))
                 breadDiscount = BasketItems.Exists(x => x.Name == "Bread") ? (BasketItems.Find(x => x.Name == "Bread").Cost * 50 / 100) : 0.00M;
 
-            return BasketItems.Sum(i => i.Cost * i.Quantity) - breadDiscount;
+            decimal milkDiscount = 0.00M;
+            if (BasketItems.Exists(x => x.Name == "Milk" && x.Quantity == 4))
+                milkDiscount = BasketItems.Find(x => x.Name == "Milk").Cost;
+
+            totalDiscount += (breadDiscount + milkDiscount);
+
+            return BasketItems.Sum(i => i.Cost * i.Quantity) - totalDiscount;
         }
 
     }
