e4537cc5650c620e70b999abac471c090a7c95a3
Paul Burkinshaw <paul.burkinshaw@gmail.com>
2017-01-10 17:35:38 +0000

    Adding BasketTotalCalculatorFactory
    BasketTotalCalculatorFactory will instantiate the relevant decorator
    objects and return the composed object, a client using this factory wont
    have to worry about how the prices are calculated. This is a very simple
    implementation, it could be refactored to use relevant price decorators
    based on values in a db or a config file etc

---
+++ PriceCalculation/BasketTotalCalculatorFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceCalculation
+{
+    public static class BasketTotalCalculatorFactory
+    {
+        public static IBasketTotalCalculator GetBasketTotalCalculator(IBasketTotalCalculator basketTotalCalculator)
+        {
+            basketTotalCalculator = new BuyTwoItemsGetDiscountDecorator("Butter", "Bread", basketTotalCalculator);
+            basketTotalCalculator = new BuyThreeItemsGetFourthItemFreeDecorator("Milk", basketTotalCalculator);
+
+            return basketTotalCalculator;
+        }
+    }
+}
--- PriceCalculation/IBasketTotalCalculator.cs
+++ PriceCalculation/IBasketTotalCalculator.cs
@@ -10,6 +10,5 @@ namespace PriceCalculation
     {
         decimal CalculateBasketTotal();
         List<BasketItem> BasketItems { get; }
-
     }
 }
